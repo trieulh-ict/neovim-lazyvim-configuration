259fe4d04a00090de2c6c76242bdd3a3c0a4b25b
Lê Hải Triều <trieulh.bk@gmail.com>
2024-11-13 22:10:55 +0700

    Update: format for js/ts/json and color highlight

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,14 +1,20 @@
 return {
-  "nvimtools/none-ls.nvim",
+  "jose-elias-alvarez/null-ls.nvim",
   config = function()
     local null_ls = require("null-ls")
+    local formatting = null_ls.builtins.formatting
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
+        null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.diagnostics.rubocop,
         null_ls.builtins.formatting.rubocop,
         null_ls.builtins.completion.spell,
+        formatting.prettier.with({
+          filetypes = { "javascript", "typescriptreact", "json" }, -- Formats .js, .tsx, and .json files
+        }),
       },
     })
 
